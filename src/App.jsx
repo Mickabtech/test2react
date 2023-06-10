8417f57995fa22547e4300f236fe84a851a6a597
agboola <mickabtech@gmail.com>
2023-06-10 13:52:30 +0100

    first commit

---
+++ src/App.jsx
@@ -0,0 +1,64 @@
+import React, { Component } from 'react';
+import UserInfo from './Component/UserInfo';
+
+class App extends Component {
+    state = {
+        userInfo: [
+          { fname: "Temitayo", lname: "Adisa", email: "tayoadisa1@gmail.com", pnum: "08035285100" },
+          { fname: "Segun", lname: "Vibes", email: "segunvibes@gmail.com", pnum: "08032295102" },
+          { fname: "Igbehiadun", lname: "Arisa", email: "igbehinadun1@gmail.com", pnum: "08045231009" },
+          { fname: "oladimi", lname: "Abesan", email: "oladimuey@gmail.com", pnum: "07089277920" },
+          { fname: "Fatai", lname: "Williams", email: "fateta@gmail.com", pnum: "081262426525" },
+        ],
+      };
+
+  render() {
+    return (
+      <div className='container'>
+        
+       <UserInfo fullname={this.state.userInfo[0].fname} 
+                 lastname={this.state.userInfo[0].lname}
+                 email = {this.state.userInfo[0].email}
+                 pnum = {this.state.userInfo[0].pnum}
+                 className = "box"
+                 
+                 />
+
+        <UserInfo fullname={this.state.userInfo[1].fname} 
+                 lastname={this.state.userInfo[1].lname}
+                 email = {this.state.userInfo[1].email}
+                 pnum = {this.state.userInfo[1].pnum}
+                className = "box"
+                 
+                 />
+
+        <UserInfo fullname={this.state.userInfo[2].fname} 
+                 lastname={this.state.userInfo[2].lname}
+                 email = {this.state.userInfo[2].email}
+                 pnum = {this.state.userInfo[2].pnum}
+                 className = "box"
+                 
+                 />
+        
+        <UserInfo fullname={this.state.userInfo[3].fname} 
+                 lastname={this.state.userInfo[3].lname}
+                 email = {this.state.userInfo[3].email}
+                 pnum = {this.state.userInfo[3].pnum}
+                 className = "box"
+                 
+                 />
+
+        <UserInfo fullname={this.state.userInfo[4].fname} 
+                 lastname={this.state.userInfo[4].lname}
+                 email = {this.state.userInfo[4].email}
+                 pnum = {this.state.userInfo[4].pnum}
+                className = "box"
+                 
+                 />
+        
+        </div>
+    )
+  }
+}
+
+export default App;
---
+++ src/Component/UserInfo.jsx
@@ -0,0 +1,14 @@
+import React from 'react'
+
+const UserInfo = (props) => {
+  return (
+    <>
+    <div>First Name: {props.fullname}</div>,
+    <div>Last Name: {props.lastname}</div>,
+    <div>Email Address: {props.email}</div>,
+    <div>Phone Number: {props.pnum}</div>,
+    </>
+  )
+}
+
+export default UserInfo
